975b0cb0f6c15fa3980b05ca856d4137cc09ff1e
ZZZWWW <zhenzhenweng@Zhenzhens-MBP.wv.cc.cmu.edu>
2015-12-01 14:56:56 -0500

    set up...

--- blockbattle-starterbot-python/Bot/Strategies/AbstractStrategy.py
+++ blockbattle-starterbot-python/Bot/Strategies/AbstractStrategy.py
@@ -3,4 +3,15 @@ class AbstractStrategy:
         self._game = game
 
     def choose(self):
-        raise NotImplementedError("Please Implement this method")
+        player = self._game.me
+        field = player.field
+        piece = self._game.piece  # piece object
+        pieceBlocks = piece.positions()
+        piecePosition = self._game.piecePosition
+        nextPiece = self._game.nextPiece
+
+        # get the frontier of the field
+        # find place to fit
+        # find corresponding moves
+        moves = []
+        return moves
