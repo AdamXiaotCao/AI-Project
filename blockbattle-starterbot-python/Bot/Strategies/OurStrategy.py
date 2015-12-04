6334a850f867595ba10cd42890eb76c9cfdf626c
shuchenc <chaoshuchenc@gmail.com>
2015-12-04 11:40:08 -0500

    With T_spin_readiness

--- blockbattle-starterbot-python/Bot/Strategies/OurStrategy.py
+++ blockbattle-starterbot-python/Bot/Strategies/OurStrategy.py
@@ -24,7 +24,7 @@ class OurStrategy(AbstractStrategy):
 
 
 
-# Heuristics
+    # Heuristics
     def bunpiness(field):
 
         pass
@@ -40,10 +40,33 @@ class OurStrategy(AbstractStrategy):
     def num_holes(field):
         pass
 
-    def I_readiness(arg):
+    def I_readiness(field):
+        # This heuristic is designed for leaving a single blank column for
+        # I piece to get combo points
+        
         pass
 
-    def T_spin(field):
+    def T_spin_readiness(self, field):
+        # This heuristic is designed for leaving a T-spin block
+        # In other words, 3 out of 4 corners of the T-shape bounding box
+        # are occupied with blocks in the field
+
+        # Find the top blocks
+        for x in xrange(1,field.width-1):
+            for y in xrange(1,field.height-1):
+                if field.field[y][x] > 1:
+                    if (field.field[y-1][x-1] > 1 and
+                        filed.field[y-1][x+1] > 1 and
+                        field.field[y-2][x-1] == 0and
+                        field.field[y-2][x+1] == 0 and
+                        (bool(field.field[y-3][x-1]) ^
+                         bool(field.field[y-3][x+1]))):
+                        return 10
+        return 0
+    
+    def T_spinning(field):
+        
         pass
+
 
 # Genetic Algorithjm
